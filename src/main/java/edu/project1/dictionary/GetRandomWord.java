f7b22b1bd5eb98928d0c5bac307c2715fd53bafd
Kirill Avdonin <kiruhaavdonin@yandex.ru>
2025-01-21 11:43:35 +0400

    `init

--- src/main/java/edu/project1/dictionary/GetRandomWord.java
+++ src/main/java/edu/project1/dictionary/GetRandomWord.java
@@ -2,7 +2,7 @@ package edu.project1.dictionary;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
 import org.jetbrains.annotations.NotNull;
@@ -15,8 +15,7 @@ public class GetRandomWord implements Dictionary {
         List<String> words;
 
         try {
-            words = Files.readAllLines(Path.of(
-                "C:/Users/kiruh/IdeaProjects/Java-course-SEA-LS/src/main/java/edu/project1/resources/dictionary.txt"));
+            words = Files.readAllLines((Paths.get("src/main/java/edu/project1/resources/Dictionary.txt")));
         } catch (IOException e) {
             throw new RuntimeException("Словарь пуст");
         }
